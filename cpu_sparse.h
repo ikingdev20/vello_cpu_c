b9e2651cd5e21db4d17dd5b0acff8d04a9435f08
ikingdev20 <9506670+ikingdev20@users.noreply.github.com>
2025-06-27 13:48:10 +0200

    Fix lints

--- cpu_sparse.h
+++ cpu_sparse.h
@@ -7,20 +7,20 @@
 #include <ostream>
 #include <new>
 
-enum class sp_fill_rule {
+enum class vc_fill_rule {
   Winding,
   EvenOdd,
 };
 
-struct sp_argb;
+struct vc_argb;
 
-struct sp_context;
+struct vc_context;
 
-struct sp_path;
+struct vc_path;
 
-struct sp_pixmap;
+struct vc_pixmap;
 
-struct sp_transform {
+struct vc_transform {
   double sx;
   double kx;
   double ky;
@@ -29,32 +29,32 @@ struct sp_transform {
   double ty;
 };
 
-struct sp_point {
+struct vc_point {
   double x;
   double y;
 };
 
-struct sp_rect {
+struct vc_rect {
   double x0;
   double y0;
   double x1;
   double y1;
 };
 
-struct sp_color {
+struct vc_color {
   uint8_t r;
   uint8_t g;
   uint8_t b;
   uint8_t a;
 };
 
-struct sp_paint {
+struct vc_paint {
   enum class Tag {
     Color,
   };
 
   struct Color_Body {
-    sp_color _0;
+    vc_color _0;
   };
 
   Tag tag;
@@ -63,69 +63,69 @@ struct sp_paint {
   };
 };
 
-struct sp_stroke {
+struct vc_stroke {
   double width;
 };
 
 extern "C" {
 
-sp_transform sp_transform_identity();
+vc_transform vc_transform_identity();
 
-sp_transform sp_transform_scale(double sx, double sy);
+vc_transform vc_transform_scale(double sx, double sy);
 
-sp_transform sp_transform_translate(double tx, double ty);
+vc_transform vc_transform_translate(double tx, double ty);
 
-sp_transform sp_transform_rotate(double angle);
+vc_transform vc_transform_rotate(double angle);
 
-sp_transform sp_transform_rotate_at(double angle, double cx, double cy);
+vc_transform vc_transform_rotate_at(double angle, double cx, double cy);
 
-sp_path *sp_path_create();
+vc_path *vc_path_create();
 
-void sp_move_to(sp_path *path, sp_point p);
+void vc_move_to(vc_path *path, vc_point p);
 
-void sp_line_to(sp_path *path, sp_point p);
+void vc_line_to(vc_path *path, vc_point p);
 
-void sp_quad_to(sp_path *path, sp_point p0, sp_point p1);
+void vc_quad_to(vc_path *path, vc_point p0, vc_point p1);
 
-void sp_cubic_to(sp_path *path, sp_point p0, sp_point p1, sp_point p2);
+void vc_cubic_to(vc_path *path, vc_point p0, vc_point p1, vc_point p2);
 
-void sp_close(sp_path *path);
+void vc_close(vc_path *path);
 
-sp_path *sp_rounded_rect(sp_rect rect, double r);
+vc_path *vc_rounded_rect(vc_rect rect, double r);
 
-void sp_path_destroy(sp_path *b);
+void vc_path_destroy(vc_path *b);
 
-sp_context *sp_context_create(uint32_t width, uint32_t height);
+vc_context *vc_context_create(uint32_t width, uint32_t height);
 
-void sp_context_destroy(sp_context *ctx);
+void vc_context_destroy(vc_context *ctx);
 
-sp_pixmap *sp_pixmap_create(uint32_t width, uint32_t height);
+vc_pixmap *vc_pixmap_create(uint32_t width, uint32_t height);
 
-void sp_pixmap_destroy(sp_pixmap *pixmap);
+void vc_pixmap_destroy(vc_pixmap *pixmap);
 
-void sp_render_to_pixmap(sp_pixmap *pixmap, sp_context *context);
+void vc_render_to_pixmap(vc_pixmap *pixmap, vc_context *context);
 
-void sp_set_transform(sp_context *ctx, sp_transform transform);
+void vc_set_transform(vc_context *ctx, vc_transform transform);
 
-void sp_set_fill_rule(sp_context *ctx, sp_fill_rule fill_rule);
+void vc_set_fill_rule(vc_context *ctx, vc_fill_rule fill_rule);
 
-void sp_set_paint(sp_context *ctx, sp_paint paint);
+void vc_set_paint(vc_context *ctx, vc_paint paint);
 
-void sp_set_stroke(sp_context *ctx, sp_stroke stroke);
+void vc_set_stroke(vc_context *ctx, vc_stroke stroke);
 
-void sp_fill_path(sp_context *ctx, const sp_path *path);
+void vc_fill_path(vc_context *ctx, const vc_path *path);
 
-void sp_stroke_path(sp_context *ctx, const sp_path *path);
+void vc_stroke_path(vc_context *ctx, const vc_path *path);
 
-void sp_fill_rect(sp_context *ctx, sp_rect rect);
+void vc_fill_rect(vc_context *ctx, vc_rect rect);
 
-sp_argb *sp_data(sp_pixmap *pixmap);
+vc_argb *vc_data(vc_pixmap *pixmap);
 
-const uint8_t *sp_argb_data(const sp_argb *data);
+const uint8_t *vc_argb_data(const vc_argb *data);
 
-void sp_argb_destroy(sp_argb *data);
+void vc_argb_destroy(vc_argb *data);
 
-void sp_stroke_rect(sp_context *ctx, sp_rect rect);
+void vc_stroke_rect(vc_context *ctx, vc_rect rect);
 
 }  // extern "C"
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
+#![allow(non_camel_case_types)]
+#![allow(clippy::missing_safety_doc)]
+
 use vello_cpu::color::{AlphaColor, Srgb};
 use vello_cpu::kurbo::{Affine, BezPath, Cap, Join, Point, Rect, RoundedRectRadii, Shape, Stroke};
 use vello_cpu::peniko::Fill;
-use vello_cpu::{Paint, PaintType, Pixmap, RenderContext, RenderMode};
+use vello_cpu::{PaintType, Pixmap, RenderContext, RenderMode};
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-pub struct sp_point {
+pub struct vc_point {
     x: f64,
     y: f64,
 }
 
-impl From<Point> for sp_point {
+impl From<Point> for vc_point {
     fn from(value: Point) -> Self {
         Self {
             x: value.x,
@@ -19,8 +22,8 @@ impl From<Point> for sp_point {
     }
 }
 
-impl From<sp_point> for Point {
-    fn from(value: sp_point) -> Self {
+impl From<vc_point> for Point {
+    fn from(value: vc_point) -> Self {
         Self {
             x: value.x,
             y: value.y,
@@ -30,7 +33,7 @@ impl From<sp_point> for Point {
 
 #[repr(C)]
 #[derive(Copy, Clone)]
-pub struct sp_transform {
+pub struct vc_transform {
     sx: f64,
     kx: f64,
     ky: f64,
@@ -39,7 +42,7 @@ pub struct sp_transform {
     ty: f64,
 }
 
-impl From<Affine> for sp_transform {
+impl From<Affine> for vc_transform {
     fn from(value: Affine) -> Self {
         let components = value.as_coeffs();
         Self {
@@ -53,61 +56,61 @@ impl From<Affine> for sp_transform {
     }
 }
 
-impl From<sp_transform> for Affine {
-    fn from(value: sp_transform) -> Self {
+impl From<vc_transform> for Affine {
+    fn from(value: vc_transform) -> Self {
         Affine::new([value.sx, value.kx, value.ky, value.sy, value.tx, value.ty])
     }
 }
 
-pub struct sp_path(BezPath);
+pub struct vc_path(BezPath);
 
 #[repr(C)]
 #[derive(Debug)]
-pub struct sp_color {
+pub struct vc_color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
 }
 
-impl From<sp_color> for AlphaColor<Srgb> {
-    fn from(value: sp_color) -> Self {
+impl From<vc_color> for AlphaColor<Srgb> {
+    fn from(value: vc_color) -> Self {
         Self::from_rgba8(value.r, value.g, value.b, value.a)
     }
 }
 
 #[repr(C)]
-pub enum sp_fill_rule {
+pub enum vc_fill_rule {
     Winding,
     EvenOdd,
 }
 
-impl From<sp_fill_rule> for Fill {
-    fn from(value: sp_fill_rule) -> Self {
+impl From<vc_fill_rule> for Fill {
+    fn from(value: vc_fill_rule) -> Self {
         match value {
-            sp_fill_rule::Winding => Fill::NonZero,
-            sp_fill_rule::EvenOdd => Fill::EvenOdd,
+            vc_fill_rule::Winding => Fill::NonZero,
+            vc_fill_rule::EvenOdd => Fill::EvenOdd,
         }
     }
 }
 
 #[repr(C)]
-pub struct sp_rect {
+pub struct vc_rect {
     pub x0: f64,
     pub y0: f64,
     pub x1: f64,
     pub y1: f64,
 }
 
-impl From<sp_rect> for Rect {
-    fn from(value: sp_rect) -> Self {
+impl From<vc_rect> for Rect {
+    fn from(value: vc_rect) -> Self {
         Rect::from_points((value.x0, value.y0), (value.x1, value.y1))
     }
 }
 
 #[no_mangle]
-pub extern "C" fn sp_transform_identity() -> sp_transform {
-    sp_transform {
+pub extern "C" fn vc_transform_identity() -> vc_transform {
+    vc_transform {
         sx: 1.0,
         kx: 0.0,
         ky: 0.0,
@@ -118,8 +121,8 @@ pub extern "C" fn sp_transform_identity() -> sp_transform {
 }
 
 #[no_mangle]
-pub extern "C" fn sp_transform_scale(sx: f64, sy: f64) -> sp_transform {
-    sp_transform {
+pub extern "C" fn vc_transform_scale(sx: f64, sy: f64) -> vc_transform {
+    vc_transform {
         sx,
         kx: 0.0,
         ky: 0.0,
@@ -130,8 +133,8 @@ pub extern "C" fn sp_transform_scale(sx: f64, sy: f64) -> sp_transform {
 }
 
 #[no_mangle]
-pub extern "C" fn sp_transform_translate(tx: f64, ty: f64) -> sp_transform {
-    sp_transform {
+pub extern "C" fn vc_transform_translate(tx: f64, ty: f64) -> vc_transform {
+    vc_transform {
         sx: 1.0,
         kx: 0.0,
         ky: 0.0,
@@ -142,156 +145,156 @@ pub extern "C" fn sp_transform_translate(tx: f64, ty: f64) -> sp_transform {
 }
 
 #[no_mangle]
-pub extern "C" fn sp_transform_rotate(angle: f64) -> sp_transform {
+pub extern "C" fn vc_transform_rotate(angle: f64) -> vc_transform {
     Affine::rotate(angle).into()
 }
 
 #[no_mangle]
-pub extern "C" fn sp_transform_rotate_at(angle: f64, cx: f64, cy: f64) -> sp_transform {
+pub extern "C" fn vc_transform_rotate_at(angle: f64, cx: f64, cy: f64) -> vc_transform {
     Affine::rotate_about(angle, Point::new(cx, cy)).into()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_path_create() -> *mut sp_path {
-    Box::into_raw(Box::new(sp_path(BezPath::new())))
+pub unsafe extern "C" fn vc_path_create() -> *mut vc_path {
+    Box::into_raw(Box::new(vc_path(BezPath::new())))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_move_to(path: *mut sp_path, p: sp_point) {
+pub unsafe extern "C" fn vc_move_to(path: *mut vc_path, p: vc_point) {
     (*path).0.move_to(p);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_line_to(path: *mut sp_path, p: sp_point) {
+pub unsafe extern "C" fn vc_line_to(path: *mut vc_path, p: vc_point) {
     (*path).0.line_to(p);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_quad_to(path: *mut sp_path, p0: sp_point, p1: sp_point) {
+pub unsafe extern "C" fn vc_quad_to(path: *mut vc_path, p0: vc_point, p1: vc_point) {
     (*path).0.quad_to(p0, p1);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_cubic_to(path: *mut sp_path, p0: sp_point, p1: sp_point, p2: sp_point) {
+pub unsafe extern "C" fn vc_cubic_to(path: *mut vc_path, p0: vc_point, p1: vc_point, p2: vc_point) {
     (*path).0.curve_to(p0, p1, p2);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_close(path: *mut sp_path) {
+pub unsafe extern "C" fn vc_close(path: *mut vc_path) {
     (*path).0.close_path();
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_rounded_rect(rect: sp_rect, r: f64) -> *mut sp_path {
+pub unsafe extern "C" fn vc_rounded_rect(rect: vc_rect, r: f64) -> *mut vc_path {
     let rect: Rect = rect.into();
     let rounded = rect.to_rounded_rect(RoundedRectRadii::from(r));
 
-    Box::into_raw(Box::new(sp_path(rounded.to_path(0.1))))
+    Box::into_raw(Box::new(vc_path(rounded.to_path(0.1))))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_path_destroy(b: *mut sp_path) {
+pub unsafe extern "C" fn vc_path_destroy(b: *mut vc_path) {
     let _ = Box::from_raw(b);
 }
 
-pub struct sp_context(RenderContext);
+pub struct vc_context(RenderContext);
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_context_create(width: u32, height: u32) -> *mut sp_context {
+pub unsafe extern "C" fn vc_context_create(width: u32, height: u32) -> *mut vc_context {
     let ctx = RenderContext::new(width as u16, height as u16);
-    Box::into_raw(Box::new(sp_context(ctx)))
+    Box::into_raw(Box::new(vc_context(ctx)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_context_destroy(ctx: *mut sp_context) {
+pub unsafe extern "C" fn vc_context_destroy(ctx: *mut vc_context) {
     let _ = Box::from_raw(ctx);
 }
 
-pub struct sp_pixmap(Pixmap);
+pub struct vc_pixmap(Pixmap);
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_pixmap_create(width: u32, height: u32) -> *mut sp_pixmap {
+pub unsafe extern "C" fn vc_pixmap_create(width: u32, height: u32) -> *mut vc_pixmap {
     let pixmap = Pixmap::new(width as u16, height as u16);
-    Box::into_raw(Box::new(sp_pixmap(pixmap)))
+    Box::into_raw(Box::new(vc_pixmap(pixmap)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_pixmap_destroy(pixmap: *mut sp_pixmap) {
+pub unsafe extern "C" fn vc_pixmap_destroy(pixmap: *mut vc_pixmap) {
     let _ = Box::from_raw(pixmap);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_render_to_pixmap(pixmap: *mut sp_pixmap, context: *mut sp_context) {
+pub unsafe extern "C" fn vc_render_to_pixmap(pixmap: *mut vc_pixmap, context: *mut vc_context) {
     (*context)
         .0
         .render_to_pixmap(&mut (*pixmap).0, RenderMode::OptimizeSpeed);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_set_transform(ctx: *mut sp_context, transform: sp_transform) {
+pub unsafe extern "C" fn vc_set_transform(ctx: *mut vc_context, transform: vc_transform) {
     (*ctx).0.set_transform(transform.into())
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_set_fill_rule(ctx: *mut sp_context, fill_rule: sp_fill_rule) {
+pub unsafe extern "C" fn vc_set_fill_rule(ctx: *mut vc_context, fill_rule: vc_fill_rule) {
     (*ctx).0.set_fill_rule(fill_rule.into());
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_set_paint(ctx: *mut sp_context, paint: sp_paint) {
+pub unsafe extern "C" fn vc_set_paint(ctx: *mut vc_context, paint: vc_paint) {
     (*ctx).0.set_paint(convert_paint(paint));
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_set_stroke(ctx: *mut sp_context, stroke: sp_stroke) {
+pub unsafe extern "C" fn vc_set_stroke(ctx: *mut vc_context, stroke: vc_stroke) {
     (*ctx).0.set_stroke(stroke.into());
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_fill_path(ctx: *mut sp_context, path: *const sp_path) {
+pub unsafe extern "C" fn vc_fill_path(ctx: *mut vc_context, path: *const vc_path) {
     (*ctx).0.fill_path(&(*path).0);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_stroke_path(ctx: *mut sp_context, path: *const sp_path) {
+pub unsafe extern "C" fn vc_stroke_path(ctx: *mut vc_context, path: *const vc_path) {
     (*ctx).0.stroke_path(&(*path).0);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_fill_rect(ctx: *mut sp_context, rect: sp_rect) {
+pub unsafe extern "C" fn vc_fill_rect(ctx: *mut vc_context, rect: vc_rect) {
     (*ctx).0.fill_rect(&rect.into());
 }
 
-pub struct sp_argb(Vec<u8>);
+pub struct vc_argb(Vec<u8>);
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_data(pixmap: *mut sp_pixmap) -> *mut sp_argb {
+pub unsafe extern "C" fn vc_data(pixmap: *mut vc_pixmap) -> *mut vc_argb {
     let mut buffer = Vec::with_capacity((*pixmap).0.data().len());
 
     for pixel in (*pixmap).0.data_as_u8_slice().chunks_exact(4) {
         buffer.extend_from_slice(&[pixel[2], pixel[1], pixel[0], pixel[3]]);
     }
 
-    Box::into_raw(Box::new(sp_argb(buffer)))
+    Box::into_raw(Box::new(vc_argb(buffer)))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_argb_data(data: *const sp_argb) -> *const u8 {
+pub unsafe extern "C" fn vc_argb_data(data: *const vc_argb) -> *const u8 {
     (*data).0.as_ptr()
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_argb_destroy(data: *mut sp_argb) {
+pub unsafe extern "C" fn vc_argb_destroy(data: *mut vc_argb) {
     let _ = Box::from_raw(data);
 }
 
 #[repr(C)]
-pub struct sp_stroke {
+pub struct vc_stroke {
     width: f64,
 }
 
-impl From<sp_stroke> for Stroke {
-    fn from(value: sp_stroke) -> Self {
+impl From<vc_stroke> for Stroke {
+    fn from(value: vc_stroke) -> Self {
         Self {
             width: value.width,
             join: Join::Bevel,
@@ -303,13 +306,13 @@ impl From<sp_stroke> for Stroke {
 }
 
 #[repr(C)]
-pub enum sp_paint {
-    Color(sp_color),
+pub enum vc_paint {
+    Color(vc_color),
 }
 
-unsafe fn convert_paint(paint: sp_paint) -> PaintType {
+unsafe fn convert_paint(paint: vc_paint) -> PaintType {
     match paint {
-        sp_paint::Color(color) => {
+        vc_paint::Color(color) => {
             let c: AlphaColor<Srgb> = color.into();
             c.into()
         }
@@ -317,6 +320,6 @@ unsafe fn convert_paint(paint: sp_paint) -> PaintType {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn sp_stroke_rect(ctx: *mut sp_context, rect: sp_rect) {
+pub unsafe extern "C" fn vc_stroke_rect(ctx: *mut vc_context, rect: vc_rect) {
     (*ctx).0.stroke_rect(&rect.into());
 }
